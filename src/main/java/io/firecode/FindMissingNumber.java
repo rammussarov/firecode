dce41ac5a20afd878896a5f1bd7b254259b0188d
rmusarov <rmusarov@nurtelecom.kg>
2019-08-13 17:17:32 +0600

    find missing number from 1 to 10

---
+++ src/main/java/io/firecode/FindMissingNumber.java
@@ -0,0 +1,23 @@
+package io.firecode;
+
+import java.util.Set;
+
+/**
+ * Given an Array containing 9 numbers ranging from 1 to 10, write a method to find the missing number.
+ * Assume you have 9 numbers between 1 to 10 and only one number is missing.
+ */
+public class FindMissingNumber {
+
+    public static void main(String[] args) {
+        System.out.println(findMissingNumber(new int[]{1, 2, 4, 5, 6, 7, 8, 9, 10}));
+    }
+
+    private static int findMissingNumber(int[] arr) {
+        int sumOfNumbers = 55;
+        int currentSum = 0;
+        for (int i : arr) {
+            currentSum += i;
+        }
+        return sumOfNumbers - currentSum;
+    }
+}
